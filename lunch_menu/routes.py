748601c8b252eead62aff78395a5ae8b5ecb1b73
Marmaduke <papa.the.roach@gmail.com>
2025-03-01 08:11:59 +0100

    Add footer

--- lunch_menu/routes.py
+++ lunch_menu/routes.py
@@ -7,32 +7,25 @@ from lunch_menu.services import LunchMenuProvider
 blueprint = Blueprint("lunch_menu")
 blueprint.static("/static", "lunch_menu/static/", name = "static")
 
-def days_of_week():
+async def menu(lunch_menu_provider: LunchMenuProvider, selected_date: date):
     now = date.today()
     monday = now - timedelta(days = now.isoweekday() - 1)
+    days_of_week = [monday + timedelta(days = i) for i in range(5)]
 
-    return [monday + timedelta(days = i) for i in range(5)]
+    return await render("index.html", context = {
+        "providers": await lunch_menu_provider.get_all_menus(),
+        "selected_date": selected_date,
+        "days_of_week": days_of_week,
+        "now": now
+    })
 
 @blueprint.get("/")
-async def index(request: Request, lunch_menu_provider: LunchMenuProvider):
-    result = await lunch_menu_provider.get_all_menus()
-    selected_date = date.today()
+def index(request: Request, lunch_menu_provider: LunchMenuProvider):
+    return menu(lunch_menu_provider, date.today())
 
-    return await render("index.html", context = {
-        "providers": result,
-        "selected_date": selected_date,
-        "days_of_week": days_of_week()
-    })
-
-@blueprint.get("/date/<selected_date:ymd>")
-async def index_date(request: Request, lunch_menu_provider: LunchMenuProvider, selected_date: date):
-    result = await lunch_menu_provider.get_all_menus()
-
-    return await render("index.html", context = {
-        "providers": result,
-        "selected_date": selected_date,
-        "days_of_week": days_of_week()
-    })
+@blueprint.get("/<selected_date:ymd>")
+def index_date(request: Request, lunch_menu_provider: LunchMenuProvider, selected_date: date):
+    return menu(lunch_menu_provider, selected_date)
 
 @blueprint.get("/api/menu")
 @cors(origin = "*")
