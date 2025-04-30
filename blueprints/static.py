14fe22328a449d8c2a88945934d0fa24ae7b58bb
Marmaduke <papa.the.roach@gmail.com>
2025-04-30 19:20:45 +0200

    Fix Pho Bo homepage

--- blueprints/static.py
+++ blueprints/static.py
@@ -2,5 +2,5 @@ from sanic import Blueprint
 
 blueprint = Blueprint("static")
 
-blueprint.static("/", "templates/index.html", name = "html")
+blueprint.static("/", "templates/index.html", name = "home")
 blueprint.static("/static", "static/", name = "static")
--- providers/phobo.py
+++ providers/phobo.py
@@ -2,7 +2,7 @@ from .base import Provider
 
 class PhoboProvider(Provider):
     name = "Pho Bo"
-    url = "https://www.facebook.com/p/PHO-BO-Restaurant-100082901603735"
+    homepage = "https://www.facebook.com/p/PHO-BO-Restaurant-100082901603735"
 
     async def get_menu(self):
         return "https://www.facebook.com/p/PHO-BO-Restaurant-100082901603735"
