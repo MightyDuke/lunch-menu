6fac12569de3a01a73869dbc48977b47746b6568
Marmaduke <papa.the.roach@gmail.com>
2025-10-09 21:26:49 +0200

    Add robots.txt

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from sanic import Sanic
+from sanic import Request, Sanic, text
 from lunch_menu.blueprint import blueprint as lunch_menu_api_blueprint
 from web.blueprint import blueprint as lunch_menu_web_blueprint
 
@@ -6,3 +6,7 @@ app = Sanic("lunch_menu", env_prefix = "LUNCH_MENU_")
 
 app.blueprint(lunch_menu_api_blueprint, url_prefix = "/api")
 app.blueprint(lunch_menu_web_blueprint)
+
+@app.get("/robots.txt")
+async def robots_txt(request: Request):
+    return text("User-agent: *\nDisallow: /")
